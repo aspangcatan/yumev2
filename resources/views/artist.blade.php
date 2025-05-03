e9f2564aeebcb7446b98bc2361416920f7918765
ceb1jadjaluddin <j.h.adjaluddin@gmail.com>
2025-05-03 09:22:50 +0800

    update
    update

--- resources/views/artist.blade.php
+++ resources/views/artist.blade.php
@@ -42,7 +42,7 @@
     <img 
       src="{{ asset('img/13.png') }}" 
       alt="Background Image" 
-      class="absolute inset-0 w-full h-[300px] sm:h-[500px] object-cover"
+      class="absolute inset-0 w-full h-[500px] object-cover"
     >
 
     <!-- Overlay -->
@@ -97,10 +97,10 @@
 <br>
     <!-- Key Components -->
     <div>
-      <h3 class="text-2xl md:text-3xl font-extrabold text-yume-orange mb-6">
+      <h3 class="text-2xl md:text-3xl font-extrabold text-yume-orange mb-3">
         Key Components
       </h3>
-      <ul class="list-disc list-inside space-y-2 text-sm text-white">
+      <ul class="list-disc pl-5 md:pl-10 space-y-2 text-sm md:text-lg text-white">
         <li><strong>Licensing:</strong> Securing licenses for composition use in various mediums, such as recordings, performances and commercial projects.</li>
         <li><strong>Royalty Collection:</strong> Ensuring songwriters and composers receive compensation for their work’s use, involving tracking and collecting royalties.</li>
         <li><strong>Rights Management:</strong> Safeguarding composition rights, including copyright protection to ensure creators’ acknowledgment and compensation.</li>
@@ -111,22 +111,26 @@
     </div>
 
     <!-- The Role of YUME -->
+    <br>
+    <br>
     <div>
-      <h3 class="text-2xl md:text-3xl font-extrabold text-yume-orange mb-6">
+      <h3 class="text-2xl md:text-3xl font-extrabold text-yume-orange mb-3">
         The Role of YUME
       </h3>
-      <p class="text-sm text-white leading-relaxed">
+      <p class="text-sm md:text-lg text-white pl-5 md:pl-10 leading-relaxed">
         YUME empowers artists with a comprehensive platform to share their music worldwide ensuring listener support and encouragement. 
         We provide cutting-edge tools for digital promotion, showcasing talent and electrifying live performances on the global stage.
       </p>
     </div>
 
     <!-- Key Benefits Include -->
+    <br>
+    <br>
     <div>
-      <h3 class="text-2xl md:text-3xl font-extrabold text-yume-orange mb-6">
+      <h3 class="text-2xl md:text-3xl font-extrabold text-yume-orange mb-3">
         Key Benefits Include
       </h3>
-      <ul class="list-disc list-inside space-y-2 text-sm text-white">
+      <ul class="list-disc pl-5 md:pl-10 space-y-2 text-sm md:text-lg text-white">
         <li><strong>Global Reach:</strong> Publishing music on over 100 platforms, including Yume, Spotify, Apple Music and TikTok.</li>
         <li><strong>Revenue Streams:</strong> Empowering fans through rewards for listening support and engagement.</li>
         <li><strong>Artist Support:</strong> Providing resources, branding managers and performance insights.</li>
@@ -144,11 +148,8 @@
 >
   <div class="relative z-10 max-w-4xl mx-auto text-center">
     <h1 class="text-4xl md:text-4xl font-extrabold leading-tight">
-      We’ll track down all of the publishing royalties
+      We’ll track down all of the publishing royalties you’re owed <span class="text-orange-500">worldwide.</span>
     </h1>
-    <h2 class="text-3xl md:text-3xl font-extrabold mt-4">
-      you’re owed <span class="text-orange-500">worldwide.</span>
-    </h2>
     <p class="text-lg md:text-xl font-medium mt-6 mb-4">
       No complicated forms. No boring legal stuff. Just more money in your pocket.
     </p>
@@ -177,7 +178,7 @@
         <!-- Block 1 -->
         <div>
           <h3 class="text-yume-orange text-2xl font-bold mb-2">Unlock Endless Opportunities</h3>
-          <ul class="list-disc pl-5 space-y-1">
+          <ul class="text-sm md:text-lg list-disc pl-5 space-y-1">
             <li>Celebrate Your Passion: Share your music with a community that acknowledges and celebrates your creativity.</li>
             <li>Recognize Your Talent: Gain the recognition you deserve for your unique sound and style.</li>
             <li>Contribute to Something Extraordinary: Every note, melody and beat contributes to a collective symphony that resonates within our vibrant community.</li>
@@ -187,7 +188,7 @@
         <!-- Block 3 -->
         <div>
           <h3 class="text-yume-orange text-2xl font-bold mb-2">Benefits of Joining</h3>
-          <ul class="list-disc pl-5 space-y-1">
+          <ul class="text-sm md:text-lg list-disc pl-5 space-y-1">
             <li>Meticulous Music Management: Trust us to handle the business side, freeing you to focus on your craft.</li>
             <li>Global Reach: Publish your music on over 100 platforms, including YUME, Spotify, Apple Music and TikTok.</li>
             <li>Revenue Streams: Empower your fans through rewards for listening support and engagement.</li>
@@ -200,7 +201,7 @@
         <!-- Block 2 -->
         <div>
             <h3 class="text-yume-orange text-2xl font-bold mb-2">Take the Leap</h3>
-            <p>
+            <p class="text-sm md:text-lg pl-5">
                 Join YUME and transform your musical aspirations into something truly exceptional. Let YUME be the stage where your music comes alive.
             </p>
         </div>
@@ -208,50 +209,50 @@
         <!-- Block 4 -->
         <div>
             <h3 class="text-yume-orange text-2xl font-bold mb-2">Embark on a Musical Odyssey</h3>
-            <p>
+            <p class="text-sm md:text-lg pl-5">
                 YUME awaits. Take the first step toward unlocking your full musical potential.
             </p>
         </div>
     </div>
 </section>
 
-<section class="relative bg-white bg-cover bg-center bg-no-repeat min-h-[120vh] flex  text-center" style="background-image: url('{{ asset('img/17.png') }}');">
+<section class="relative bg-white bg-cover bg-center bg-no-repeat min-h-[120vh] flex text-center px-4 pb-4 sm:px-0 sm:pb-0" style="background-image: url('{{ asset('img/17.png') }}');">
 
-<div class="max-w-xl w-fit h-fit ml-0 sm:ml-20 mt-12 p-8 border-2 border-black bg-white/10 backdrop-blur-md shadow-xl rounded-lg">
-  <h1 class="text-2xl font-extrabold text-orange-600 uppercase">Artist Submission Form</h1>
-  <h2 class="text-lg font-bold mt-1 mb-4 text-gray-800">Embark on a Musical Odyssey!</h2>
-  <p class="text-gray-800 mb-2 text-xs">
-    Ready to take your musical journey to unprecedented heights? Yume eagerly extends an invitation for you to express your interest in becoming part of our dynamic community, entrusting us with the meticulous management of your music.
-  </p>
-  <p class="text-gray-800 mb-2 text-xs">
-    Whether you’re a visionary artist, a skilled songwriter, or an impassioned music enthusiast, Yume opens its doors to a world where your passion isn’t just acknowledged; it’s celebrated.
-  </p>
-  <p class="text-gray-800 mb-2 text-xs">
-    Take the leap and join Yume, where every note, every melody, and every beat contributes to something extraordinary!
-  </p>
+  <div class="max-w-xl w-fit h-fit ml-0 sm:ml-20 mt-8 mb-8 p-8 border-2 border-black bg-white/10 backdrop-blur-md shadow-xl rounded-lg">
+    <h1 class="text-2xl font-extrabold text-orange-600 uppercase">Artist Submission Form</h1>
+    <h2 class="text-lg font-bold mt-1 mb-4 text-gray-800">Embark on a Musical Odyssey!</h2>
+    <p class="text-gray-800 mb-2 text-sm md:text-lg">
+      Ready to take your musical journey to unprecedented heights? Yume eagerly extends an invitation for you to express your interest in becoming part of our dynamic community, entrusting us with the meticulous management of your music.
+    </p>
+    <p class="text-gray-800 mb-2 text-sm md:text-lg">
+      Whether you’re a visionary artist, a skilled songwriter, or an impassioned music enthusiast, Yume opens its doors to a world where your passion isn’t just acknowledged; it’s celebrated.
+    </p>
+    <p class="text-gray-800 mb-2 text-sm md:text-lg">
+      Take the leap and join Yume, where every note, every melody, and every beat contributes to something extraordinary!
+    </p>
 
-  <form class="space-y-2">
-    <input type="text" placeholder="Name*" class="w-full px-4 py-2 border border-black placeholder-gray-500 bg-white/80 focus:outline-none focus:ring-2 focus:ring-orange-400 text-gray-800" />
-    <input type="email" placeholder="Email Address*" class="w-full px-4 py-2 border border-black placeholder-gray-500 bg-white/80 focus:outline-none focus:ring-2 focus:ring-orange-400 text-gray-800" />
-    <input type="tel" placeholder="Phone*" class="w-full px-4 py-2 border border-black placeholder-gray-500 bg-white/80 focus:outline-none focus:ring-2 focus:ring-orange-400 text-gray-800" />
-    <select class="w-full px-4 py-2 border border-black bg-white/80 text-gray-800 focus:outline-none focus:ring-2 focus:ring-orange-400">
-      <option disabled selected>Please select country*</option>
-      <option>United States</option>
-      <option>Canada</option>
-      <option>United Kingdom</option>
-      <option>Australia</option>
-      <option>Germany</option>
-      <option>India</option>
-      <option>Japan</option>
-    <!-- Add more as needed -->
-    </select>
-    <textarea placeholder="Message*" rows="4" class="w-full px-4 py-2 border border-black placeholder-gray-500 bg-white/80 focus:outline-none focus:ring-2 focus:ring-orange-400 text-gray-800"></textarea>
-        <!-- Submit Button -->
-  <button type="submit" class="w-full bg-orange-700 text-white font-bold py-3 px-6 rounded-lg hover:bg-orange-900 transition">
-    Submit
-  </button>
-  </form>
-</div>
+    <form class="space-y-2">
+      <input type="text" placeholder="Name*" class="w-full px-4 py-2 border border-black placeholder-gray-500 bg-white/80 focus:outline-none focus:ring-2 focus:ring-orange-400 text-gray-800" />
+      <input type="email" placeholder="Email Address*" class="w-full px-4 py-2 border border-black placeholder-gray-500 bg-white/80 focus:outline-none focus:ring-2 focus:ring-orange-400 text-gray-800" />
+      <input type="tel" placeholder="Phone*" class="w-full px-4 py-2 border border-black placeholder-gray-500 bg-white/80 focus:outline-none focus:ring-2 focus:ring-orange-400 text-gray-800" />
+      <select class="w-full px-4 py-2 border border-black bg-white/80 text-gray-800 focus:outline-none focus:ring-2 focus:ring-orange-400">
+        <option disabled selected>Please select country*</option>
+        <option>United States</option>
+        <option>Canada</option>
+        <option>United Kingdom</option>
+        <option>Australia</option>
+        <option>Germany</option>
+        <option>India</option>
+        <option>Japan</option>
+      <!-- Add more as needed -->
+      </select>
+      <textarea placeholder="Message*" rows="4" class="w-full px-4 py-2 border border-black placeholder-gray-500 bg-white/80 focus:outline-none focus:ring-2 focus:ring-orange-400 text-gray-800"></textarea>
+          <!-- Submit Button -->
+    <button type="submit" class="w-full bg-orange-700 text-white font-bold py-3 px-6 rounded-lg hover:bg-orange-900 transition">
+      Submit
+    </button>
+    </form>
+  </div>
 
 </section>
 
